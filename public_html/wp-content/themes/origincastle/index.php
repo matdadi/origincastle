14f908445ef43537513324ebb08e554a206767d6
matdadi <drahmat11@gmail.com>
2025-11-16 22:07:50 +0700

    add origincastle folder

---
+++ public_html/wp-content/themes/origincastle/index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+        <main id="content" class="content">
+            <section id="banner">banner</section>
+            <section id="top-product">unggulan</section>
+            <section id="popular-product">sedang naik</section>
+        </main>
+<?php get_footer(); ?>
