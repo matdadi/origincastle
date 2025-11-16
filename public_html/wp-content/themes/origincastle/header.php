13aabe5b49a7e61304b86d58bc4a5af93153c056
matdadi <drahmat11@gmail.com>
2025-11-16 21:54:26 +0700

    Reapply "add themes only"
    This reverts commit ffd4c47a30d23898d3914bdd3617741f5bdb7f70.

---
+++ public_html/wp-content/themes/origincastle/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>
+    <div id="page" class="page">
+        <header>
+            <section class="menu">
+                <div id="title" class="title">
+                    <a>origincastlebookstore.com</a>
+                </div>
+                <div id="navigation" class="navigation">
+                    <nav>etalase</nav>
+                    <nav>promosi</nav>
+                </div>
+            </section>
+        </header>
